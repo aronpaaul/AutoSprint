cf0088639b64da69c208d7034367f07efe3603bf
aron-paul <y.a.irh.m.ir.sn.d.a.4.1.7@gmail.com>
2026-05-02 20:07:04 +0300

    Initial commit

---
+++ src/main/java/ru/paul/autosprint/AutoSprintClient.java
@@ -0,0 +1,23 @@
+package ru.paul.autosprint;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+
+public final class AutoSprintClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register(AutoSprintClient::forceSprint);
+        ClientTickEvents.END_CLIENT_TICK.register(AutoSprintClient::forceSprint);
+    }
+
+    private static void forceSprint(MinecraftClient client) {
+        ClientPlayerEntity player = client.player;
+        if (player == null || !player.isAlive() || player.isSpectator()) {
+            return;
+        }
+
+        player.setSprinting(true);
+    }
+}
